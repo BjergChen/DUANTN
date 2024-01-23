d3240b687eea492a87141f4871e10b4f62237e60
Taoru1609 <nguyenntph24375@fpt.edu.vn>
2024-01-23 20:41:03 +0700

    push BE

---
+++ Source/Back-End/DUATN/src/test/java/com/example/DUATN/DuatnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DUATN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuatnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
